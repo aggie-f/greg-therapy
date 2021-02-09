b900ac289e4cab9d37fc85a9407e0233def74186
Agata Filus <agatafilus@gmail.com>
2021-02-09 23:44:55 +0000

    controller and view for article index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,3 +1,14 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  # GET /articles
+  def index
+    @articles = Article.all
+  end
+
+  # GET /articles/1
+  def show
+  end
+
 
 end
