bd5ae502cdfcd474d6dfc30ed771a30ac1c5edf6
Agata Filus <agatafilus@gmail.com>
2021-02-10 00:11:50 +0000

    controller and view for new and show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,5 +10,27 @@ class ArticlesController < ApplicationController
   def show
   end
 
+  def new
+    @article = Article.new
+  end
 
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article, notice: 'Article was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    def article_params
+      params.require(:article).permit(:title, :rich_body, photos: [])
+    end
 end
