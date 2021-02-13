aae4c54a9464a8ef8de39d327fa5e6d522640d06
Agata Filus <agatafilus@gmail.com>
2021-02-13 21:15:15 +0000

    new style for navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,14 +24,18 @@ require("channels")
 
 // External imports
 import "bootstrap";
-
+import { navbar } from '../components/navbar';
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  toggleButton();
+  // navbarLinks();
+
 });
+
 
 require("trix")
 require("@rails/actiontext")
