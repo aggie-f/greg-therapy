1d24e0ce1df61a4d73a57bb6a0862223b4761e9c
Agata Filus <agatafilus@gmail.com>
2021-02-16 23:57:15 +0000

    routes renamed and linkes added to navbar

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,8 +1,8 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
-toggleButton.addEventListener('click', () => {
+export const hamburger = () => {
+  const toggleButton = document.getElementsByClassName('toggle-button')[0]
+  const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+  toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 });
-
-export { toggleButton }
+}
