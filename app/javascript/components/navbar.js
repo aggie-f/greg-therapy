1ab2f4e9b80286e223b8fc8d1b8ede23c15e19b8
Agata Filus <agatafilus@gmail.com>
2021-03-19 00:36:32 +0000

    toggle btn fixed

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,8 +1,8 @@
-
 export const hamburger = () => {
   const toggleButton = document.getElementsByClassName('toggle-button')[0]
   const navbarLinks = document.getElementsByClassName('navbar-links')[0]
   toggleButton.addEventListener('click', () => {
+  event.preventDefault();
   navbarLinks.classList.toggle('active');
   });
 }
