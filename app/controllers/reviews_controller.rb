188551a2e94dc8ffe2202014250ad44e431be4ed
Agata Filus <agatafilus@gmail.com>
2021-03-19 15:43:44 +0000

    review controller new, create

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,16 @@
+class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.save
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
+end
