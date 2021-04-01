4b639523bcf969b3013ad08fd797f562774e67e8
Agata Filus <agatafilus@gmail.com>
2021-04-01 21:16:22 +0100

    admin page

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   get 'cennik', to: 'pages#cennik'
   get 'blog', to: 'pages#articles'
   get 'kontakt', to: 'pages#kontakt'
+  get 'admin', to: 'pages#admin'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
