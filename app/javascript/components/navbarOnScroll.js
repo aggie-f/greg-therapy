c41f66d120e8eab8d13bc7447d6df23fd1d93f8c
Agata Filus <agatafilus@gmail.com>
2021-06-03 15:52:38 +0100

    JS navbar on scroll needs to be fixed

---
+++ app/javascript/components/navbarOnScroll.js
@@ -0,0 +1,24 @@
+// const header = document.querySelector(".navbar-home");
+// const sectionOne = document.querySelector("#section02");
+
+// const sectionOneOptions = {
+//   rootMargin: "-200px 0px 0px 0px"
+// };
+
+// const sectionOneObserver = new IntersectionObserver(function(
+//   entries,
+//   sectionOneObserver
+// ) {
+//   entries.forEach(entry => {
+//     if (!entry.isIntersecting) {
+//       header.classList.add("nav-scrolled");
+//     } else {
+//       header.classList.remove("nav-scrolled");
+//     }
+//   });
+// },
+// sectionOneOptions);
+
+// sectionOneObserver.observe(sectionOne);
+
+// export { sectionOneObserver };
